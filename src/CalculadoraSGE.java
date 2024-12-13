7e461dc319bc33957e87d7ebba1b616cb58f09c8
BlackClover03 <dl26089@gmail.com>
2024-12-13 18:40:06 +0100

    Tercera version Calculadora.

--- src/CalculadoraSGE.java
+++ src/CalculadoraSGE.java
@@ -15,7 +15,7 @@ public class CalculadoraSGE {
         return M;
     }
 
-    public static double division(Integer num1, Integer num2) {
+    public static double Calculardivision(Integer num1, Integer num2) {
         if (num2 == 0) {
             throw new IllegalArgumentException("No se puede dividir entre 0.");
         }
--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("Suma: "+ num1 + " + " + num2 + " = " + CalculadoraSGE.suma(num1, num2));
         System.out.println("Resta: " + num1 + " - " + num2 + " = " + CalculadoraSGE.resta(num1, num2));
         System.out.println("Multiplicación: " + num1 + " * " + num2 + " = " + CalculadoraSGE.multiplicacion(num1, num2));
-        System.out.println("División: " + num1 + " / " + num2 + " = " + CalculadoraSGE.division(num1, num2));
+        System.out.println("División: " + num1 + " / " + num2 + " = " + CalculadoraSGE.Calculardivision(num1, num2));
         System.out.println("Raiz Cuadrada de: " + num3 + " = " + CalculadoraSGE.CalcularRaizCuadrada(num3));
     }
 }
